8a12472331ec1a1139be0fc4829f1b5283b09d39
Maciej Zonski <me@zonni.pl>
2023-05-14 16:57:24 +0200

    1. Two Sum

---
+++ leetcode-problems/Problems/1.TwoSum.cs
@@ -0,0 +1,38 @@
+﻿// Problem: Two Sum
+// Difficulty: Easy
+// Link: https://leetcode.com/problems/two-sum/
+
+namespace LeetCode.Problems.Problems;
+
+public class P1TwoSum
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        for (var i = 0; i < nums.Length; i++)
+        {
+            for (var j = 0; j < nums.Length; j++)
+            {
+                if (i == j) continue;
+                if (nums[i] + nums[j] == target) return new[] { i, j };
+            }
+        }
+        throw new Exception("Not found");
+    }
+    
+    public int[] TwoSum2(int[] nums, int target)
+    {
+        var numbers = new Dictionary<int, int>();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var complement = target - nums[i];
+            if (numbers.ContainsKey(complement))
+                return new[] { numbers[complement], i };
+
+            if (!numbers.ContainsKey(nums[i]))
+                numbers.Add(nums[i], i);
+        }
+
+        throw new Exception("Not found");
+    }
+}
---
+++ leetcode-tests/Problems/1.TwoSum.cs
@@ -0,0 +1,33 @@
+﻿using LeetCode.Problems.Problems;
+
+namespace LeetCode.Tests.Problems;
+
+public class P1TwoSumTest
+{
+    private readonly P1TwoSum _class;
+
+    public P1TwoSumTest()
+    {
+        _class = new P1TwoSum();
+    }
+
+    [Theory]
+    [InlineData(new[] { 2, 7, 11, 15 }, 9, new[] { 0, 1 })]
+    [InlineData(new[] { 3, 2, 4 }, 6, new[] { 1, 2 })]
+    [InlineData(new[] { 3, 3 }, 6, new[] { 0, 1 })]
+    public void TwoSum_ReturnsExpectedResult(int[] nums, int target, int[] expectedResult)
+    {
+        var result = _class.TwoSum(nums, target);
+        Assert.Equal(expectedResult, result);
+    }
+
+    [Theory]
+    [InlineData(new[] { 2, 7, 11, 15 }, 9, new[] { 0, 1 })]
+    [InlineData(new[] { 3, 2, 4 }, 6, new[] { 1, 2 })]
+    [InlineData(new[] { 3, 3 }, 6, new[] { 0, 1 })]
+    public void TwoSum2_ReturnsExpectedResult(int[] nums, int target, int[] expectedResult)
+    {
+        var result = _class.TwoSum2(nums, target);
+        Assert.Equal(expectedResult, result);
+    }
+}
