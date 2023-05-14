8a2e05d0e9a7cca6ff54d6bbbef0a417bb3108ec
Maciej Zonski <me@zonni.pl>
2023-05-14 16:27:55 +0200

    242. Valid Anagram

---
+++ leetcode-problems/Problems/242.ValidAnagram.cs
@@ -0,0 +1,71 @@
+﻿// Problem: Valid Anagram
+// Difficulty: Easy
+// Link: https://leetcode.com/problems/valid-anagram/
+
+namespace LeetCode.Problems.Problems;
+
+public class P242ValidAnagram
+{
+    public bool IsAnagram(string s, string t)
+    {
+        return s.Length == t.Length && s.OrderBy(a => a).SequenceEqual(t.OrderBy(a => a));
+    }
+
+    public bool IsAnagram2(string s, string t)
+    {
+        if (s.Length != t.Length) return false;
+
+        var sArr = s.OrderBy(a => a).ToArray();
+        var tArr = t.OrderBy(a => a).ToArray();
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (sArr[i] != tArr[i]) return false;
+        }
+
+        return true;
+    }
+
+    public bool IsAnagram3(string s, string t)
+    {
+        if (s.Length != t.Length) return false;
+
+        var stats = new int[26];
+
+        foreach (var sChar in s)
+        {
+            stats[sChar - 'a']++;
+        }
+
+        foreach (var tChar in t)
+        {
+            stats[tChar - 'a']--;
+
+            if (stats[tChar - 'a'] < 0)
+                return false;
+        }
+
+        return true;
+    }
+    
+    public bool IsAnagram4(string s, string t)
+    {
+        if (s.Length != t.Length) return false;
+
+        var stats = new int[26];
+        Array.Fill(stats, 0);
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            stats[s[i] - 'a']++;
+            stats[t[i] - 'a']--;
+        }
+
+        for (var i = 0; i < 26; i++)
+        {
+            if (stats[i] != 0) return false;
+        }
+
+        return true;
+    }
+}
---
+++ leetcode-tests/Problems/242.ValidAnagram.cs
@@ -0,0 +1,49 @@
+﻿using LeetCode.Problems.Problems;
+
+namespace LeetCode.Tests.Problems;
+
+public class P242ValidAnagramTest
+{
+    private readonly P242ValidAnagram _class;
+
+    public P242ValidAnagramTest()
+    {
+        _class = new P242ValidAnagram();
+    }
+
+    [Theory]
+    [InlineData("anagram", "nagaram", true)]
+    [InlineData("rat", "car", false)]
+    public void IsAnagram_ReturnsExpectedResult(string s, string t, bool expectedResult)
+    {
+        var result = _class.IsAnagram(s, t);
+        Assert.Equal(expectedResult, result);
+    }
+
+    [Theory]
+    [InlineData("anagram", "nagaram", true)]
+    [InlineData("rat", "car", false)]
+    public void IsAnagram2_ReturnsExpectedResult(string s, string t, bool expectedResult)
+    {
+        var result = _class.IsAnagram2(s, t);
+        Assert.Equal(expectedResult, result);
+    }
+    
+    [Theory]
+    [InlineData("anagram", "nagaram", true)]
+    [InlineData("rat", "car", false)]
+    public void IsAnagram3_ReturnsExpectedResult(string s, string t, bool expectedResult)
+    {
+        var result = _class.IsAnagram3(s, t);
+        Assert.Equal(expectedResult, result);
+    }
+    
+    [Theory]
+    [InlineData("anagram", "nagaram", true)]
+    [InlineData("rat", "car", false)]
+    public void IsAnagram4_ReturnsExpectedResult(string s, string t, bool expectedResult)
+    {
+        var result = _class.IsAnagram4(s, t);
+        Assert.Equal(expectedResult, result);
+    }
+}
