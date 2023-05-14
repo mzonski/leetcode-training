e7c202c6499017136d9b72cca36aee192ff46e79
Maciej Zonski <me@zonni.pl>
2023-05-14 16:16:07 +0200

    217. Contains Duplicate

---
+++ leetcode-problems/Problems/217.ContainsDuplicate.cs
@@ -0,0 +1,31 @@
+﻿// Problem: Contains Duplicate
+// Difficulty: Easy
+// Link: https://leetcode.com/problems/contains-duplicate/
+
+namespace LeetCode.Problems.Problems;
+
+public class P217ContainsDuplicate
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        var set = new HashSet<int>();
+
+        foreach (var num in nums)
+        {
+            if (set.Contains(num))
+            {
+                return true;
+            }
+
+            set.Add(num);
+        }
+
+        return false;
+    }
+
+    public bool ContainsDuplicate2(int[] nums)
+    {
+        var set = new HashSet<int>(nums);
+        return nums.Length != set.Count;
+    }
+}
---
+++ leetcode-tests/Problems/217.ContainsDuplicate.cs
@@ -0,0 +1,35 @@
+﻿using LeetCode.Problems.Problems;
+
+namespace LeetCode.Tests.Problems;
+
+public class ContainsDuplicate217Test
+{
+    private readonly P217ContainsDuplicate _containsDuplicate;
+
+    public ContainsDuplicate217Test()
+    {
+        _containsDuplicate = new P217ContainsDuplicate();
+    }
+
+    [Theory]
+    [InlineData(new[] { 1, 2, 3, 1 }, true)]
+    [InlineData(new[] { 1, 2, 3, 4 }, false)]
+    [InlineData(new[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 }, true)]
+    [InlineData(new int[] { }, false)]
+    public void ContainsDuplicate_ReturnsExpectedResult(int[] nums, bool expectedResult)
+    {
+        var result = _containsDuplicate.ContainsDuplicate(nums);
+        Assert.Equal(expectedResult, result);
+    }
+
+    [Theory]
+    [InlineData(new[] { 1, 2, 3, 1 }, true)]
+    [InlineData(new[] { 1, 2, 3, 4 }, false)]
+    [InlineData(new[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 }, true)]
+    [InlineData(new int[] { }, false)]
+    public void ContainsDuplicate2_ReturnsExpectedResult(int[] nums, bool expectedResult)
+    {
+        var result = _containsDuplicate.ContainsDuplicate2(nums);
+        Assert.Equal(expectedResult, result);
+    }
+}
